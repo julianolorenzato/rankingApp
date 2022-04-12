f7bb889a6ba7710f90bbc20c7eaf1c624660476b
Juliano Lorenzato <julianolorenzato@gmail.com>
2022-04-12 18:41:57 -0300

    project setup

---
+++ packages/web/src/pages/_app.jsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from 'styled-components'
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<ThemeProvider theme={{}}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
+}
+
+export default MyApp
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	useTabs: true,
+	printWidth: 80,
+	semi: false,
+	tabWidth: 4,
+	singleQuote: true,
+	trailingComma: 'none',
+	arrowParens: 'avoid',
+	endOfLine: 'auto'
+}
