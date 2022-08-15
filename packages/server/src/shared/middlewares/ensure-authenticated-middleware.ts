abe7a62e382a28ce7ae703fea60de8087fb8db19
Juliano Lorenzato <julianolorenzato@gmail.com>
2022-08-14 21:35:49 -0300

    styles: remove unused imports

--- packages/server/src/shared/middlewares/ensure-authenticated-middleware.ts
+++ packages/server/src/shared/middlewares/ensure-authenticated-middleware.ts
@@ -1,34 +1,31 @@
-import { InvalidJWTTokenError } from "modules/accounts/services/errors/invalid-jwt-token-error";
-import { JWT } from "modules/accounts/services/jwt-service";
-import { HttpResponse } from "shared/contracts/infra/http-response";
-import { Middleware } from "shared/contracts/infra/middleware";
-import { InvalidAccessTokenError } from "shared/errors/invalid-access-token-error";
-import { NotFoundError } from "shared/errors/not-found-error";
-import { UnauthorizedError } from "shared/errors/unauthorized-error";
+import { JWT } from 'modules/accounts/services/jwt-service'
+import { HttpResponse } from 'shared/contracts/infra/http-response'
+import { Middleware } from 'shared/contracts/infra/middleware'
+import { InvalidAccessTokenError } from 'shared/errors/invalid-access-token-error'
+import { UnauthorizedError } from 'shared/errors/unauthorized-error'
 
 type RequestData = {
-    accessToken: string
+	accessToken: string
 }
 
 class EnsureAuthenticatedMiddleware extends Middleware<any> {
-    protected async handle(requestData: RequestData): Promise<HttpResponse> {
-        const { accessToken } = requestData
+	protected async handle(requestData: RequestData): Promise<HttpResponse> {
+		const { accessToken } = requestData
 
-        if (!accessToken) {
-            return this.unauthorized(new InvalidAccessTokenError())
-        }
+		if (!accessToken) {
+			return this.unauthorized(new InvalidAccessTokenError())
+		}
 
-        const jwt = JWT.verifyUserToken(accessToken)
-    
-        if (jwt.isRight()) {
-            const { payload } = jwt.value
+		const jwt = JWT.verifyUserToken(accessToken)
 
-            return this.ok(payload)
-        } else {
-            return this.unauthorized(new UnauthorizedError())
-        }
+		if (jwt.isRight()) {
+			const { payload } = jwt.value
 
-    }
+			return this.ok(payload)
+		} else {
+			return this.unauthorized(new UnauthorizedError())
+		}
+	}
 }
 
 const ensureAuthenticatedMiddleware = new EnsureAuthenticatedMiddleware()
